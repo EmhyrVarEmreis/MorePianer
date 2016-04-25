bdf6c565cb6fee28130e8c61e1db917430a27496
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2016-04-25 22:49:00 +0200

    Clean-Up

--- app/src/main/java/pl/morecraft/dev/morepianer/app/DesktopApplicationCopy.java
+++
@@ -1,9 +0,0 @@
-package pl.morecraft.dev.morepianer.app;
-
-public class DesktopApplicationCopy {
-
-    public static void main(String[] args) {
-        System.out.println("This is MorePianer desktop application");
-    }
-
-}
